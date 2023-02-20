895e651016eb187efa7ba4de67b2274bc454e8e9
Jairo Valderrama <jairovg@gmail.com>
2023-02-20 14:47:42 -0500

    feat(@fem-rsc/tokens): build tokens to export them

---
+++ packages/tokens/transforms/color-css.mjs
@@ -0,0 +1,7 @@
+// For backwards compatibility, all built-in transforms are not transitive
+// by default. This will make the 'color/css' transform transitive
+export default (StyleDictionary) =>
+  Object.assign({}, StyleDictionary.transform['color/css'], {
+    name: 'color/css',
+    transitive: true,
+  });
